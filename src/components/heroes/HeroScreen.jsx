c3736518c9a5ab7823f2c583ebe71a8b99cc1a5d
makadown <maseflores79@gmail.com>
2021-04-24 19:06:06 -0500

    Estilo de componente HeroScreen

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router';
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
   // extraigo los parametros del url
   const { heroeId } = useParams();
   const hero = getHeroById(heroeId);
   if (!hero) {
-      return <Redirect to="/"/>
+    return <Redirect to="/" />;
   }
+  const handleReturn = () => {
+    const page = publisher.toLowerCase().replace(' comics', '');
+    console.log(page);
+    
+    history.replace('/'+page);
+  };
   const {
     id,
     superhero,
@@ -19,8 +25,40 @@ export const HeroScreen = () => {
   } = hero;
 
   return (
-    <div>
-      <h1>Hero Screen</h1>
+    <div className="row mt-2">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8 text-white bg-dark">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush  text-white bg-dark">
+          <li className="list-group-item text-white bg-dark">
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+          <li className="list-group-item text-white bg-dark">
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className="list-group-item text-white bg-dark">
+            <b>First Appereance: </b>
+            {first_appearance}
+          </li>
+          <li className="list-group-item text-white bg-dark">
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+        </ul>
+        <h5> Characters </h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info mb-3"
+              onClick={handleReturn}>
+                Return</button>
+      </div>
     </div>
   );
 };
