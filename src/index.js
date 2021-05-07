f6ad0e8c8f530c58467d5e0552414b12f8d00c1c
makadown <maseflores79@gmail.com>
2021-05-06 22:21:04 -0500

    correccion en rutas de imagenes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
 import './index.css';
 
-ReactDOM.render( 
-    <HeroesApp />,
+ReactDOM.render(
+  <HashRouter>
+    <HeroesApp />
+  </HashRouter>,
   document.getElementById('root')
 );
 
